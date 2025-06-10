ee728b211fbe2614cafb34c523f56677da130f56
eldaTacenter <darioguaraz@gmail.com>
2025-06-10 01:14:27 -0300

    portfolio v2.0

--- main.js
+++ main.js
@@ -31,34 +31,3 @@ document.addEventListener("DOMContentLoaded", function () {
   toggleContent("#experience", "UL");
 });
 //-------------------------------------carousel info---------------
-
-//--------------boton tema oscruo---------------------------//
-
-document.addEventListener("DOMContentLoaded", () => {
-  const darkModeToggle = document.getElementById("darkModeToggle");
-
-  if (darkModeToggle) {
-    darkModeToggle.addEventListener("click", (e) => {
-      e.preventDefault();
-
-      // Alterna la clase 'dark-mode' en el body
-      document.body.classList.toggle("dark-mode");
-
-      // Alterna la clase 'dark-mode' en otros elementos
-      document
-        .querySelectorAll(
-          "header, h3, section, #text-about, .footer, #education, #experience, #projects, #about .navbar-nav .nav-link"
-        )
-        .forEach((element) => {
-          element.classList.toggle("dark-mode");
-        });
-
-      // Cambia el texto del botón según el estado
-      darkModeToggle.textContent = document.body.classList.contains("dark-mode")
-        ? "☀️"
-        : "🌙";
-    });
-  } else {
-    console.error("El elemento con id='darkModeToggle' no fue encontrado.");
-  }
-});
